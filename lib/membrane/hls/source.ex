ad6232899bf856ff5243caf99363b1da7c2e5023
Daniel Morandini <danielmorandini@me.com>
2022-07-19 18:45:40 +0200

    Add Membrane.Source stub

--- lib/membrane/hls/source.ex
+++ lib/membrane/hls/source.ex
@@ -1,3 +1,28 @@
 defmodule Membrane.HLS.Source do
   use Membrane.Source
+
+  def_output_pad(:output, [
+    mode: :pull,
+    caps: :any,
+  ])
+
+  def_options([
+    storage: [spec: HLS.Storage.t(), description: "HLS.Storage instance pointing to the target HLS playlist"],
+    target: [spec: HLS.AlternativeRendition.t() | HLS.VariantStream.t(), description: "Stream to be followed"],
+  ])
+
+  @impl true
+  def handle_init(options) do
+    {:ok, %{storage: options.storage, target: options.target}}
+  end
+
+  @impl true
+  def handle_stopped_to_prepared(_ctx, state) do
+    {{:ok, [caps: {:output, :any}]}, state}
+  end
+
+  @impl true
+  def handle_demand(:output, _size, :buffers, _ctx, state) do
+    {{:ok, [{:end_of_stream, :output}]}, state}
+  end
 end
---
+++ test/membrane/hls/source_test.exs
@@ -0,0 +1,39 @@
+defmodule Membrane.HLS.SourceTest do
+  use ExUnit.Case
+
+  alias Membrane.HLS.Source
+  alias Membrane.Testing
+  alias HLS.Storage
+  alias HLS.Playlist.Master
+
+  import Membrane.Testing.Assertions
+
+  @master_playlist_path "./fixtures/mpeg-ts/stream.m3u8"
+  @store Storage.new(%Storage.FS{location: @master_playlist_path})
+
+  describe "hls source" do
+    test "provides all segments of selected stream" do
+      stream =
+        @store
+        |> Storage.get_master_playlist!()
+        |> Master.variant_streams()
+        |> List.first()
+
+      children = [
+        source: %Source{storage: @store, target: stream},
+        sink: %Testing.Sink{}
+      ]
+
+      {:ok, pid} = Testing.Pipeline.start_link(
+        links: Membrane.ParentSpec.link_linear(children)
+      )
+
+      # assert_sink_buffer(pid, :sink ,%Membrane.Buffer{payload: 255})
+      # assert_sink_buffer(pid, :sink ,%Membrane.Buffer{payload: 255})
+      # assert_sink_buffer(pid, :sink ,%Membrane.Buffer{payload: 255})
+      # assert_sink_buffer(pid, :sink ,%Membrane.Buffer{payload: 255})
+      # assert_sink_buffer(pid, :sink ,%Membrane.Buffer{payload: 255})
+      assert_end_of_stream(pid, :sink)
+    end
+  end
+end
