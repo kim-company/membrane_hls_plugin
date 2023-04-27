680ec2bcf12f220a85603740821a5060fd98e10b
Daniel Morandini <danielmorandini@me.com>
2023-04-27 11:19:19 +0200

    Update hls library

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Membrane.HLS.MixProject do
     [
       {:membrane_core, "~> 0.11.0"},
       {:membrane_file_plugin, "~> 0.13.2", only: :test},
-      {:kim_hls, path: "../kim_hls"},
+      {:kim_hls, github: "kim-company/kim_hls"},
       {:kim_q, github: "kim-company/kim_q"}
     ]
   end
