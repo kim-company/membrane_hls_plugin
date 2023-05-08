28c3b4f036938e609b7a5b091d0c5d2db402131f
Daniel Morandini <danielmorandini@me.com>
2023-05-08 11:59:56 +0200

    Improve test coverage

--- test/membrane/hls/sink_test.exs
+++ test/membrane/hls/sink_test.exs
@@ -1,31 +1,10 @@
 defmodule Membrane.HLS.SinkTest do
   use ExUnit.Case
+  use Membrane.Pipeline
 
   alias HLS.Playlist.Media
   alias Membrane.Buffer
   import Membrane.Testing.Assertions
-
-  defmodule Pipeline do
-    use Membrane.Pipeline
-
-    @impl true
-    def handle_init(_ctx, %{
-          playlist: playlist,
-          output: output
-        }) do
-      structure = [
-        child(:source, %Membrane.Testing.Source{
-          output: output,
-          stream_format: %Membrane.HLS.Format.WebVTT{language: "en-US"}
-        })
-        |> child(:sink, %Membrane.HLS.Sink{
-          playlist: playlist
-        })
-      ]
-
-      {[{:spec, structure}, {:playback, :playing}], %{}}
-    end
-  end
 
   def buffer_generator(buffers, size) do
     {buffers, rest} = Enum.split(buffers, size)
@@ -47,36 +26,34 @@ defmodule Membrane.HLS.SinkTest do
   end
 
   describe "hls sink" do
-    test "works with one segment" do
+    test "handles one segment" do
       playlist_uri = URI.new!("s3://bucket/media.m3u8")
 
-      options = [
-        module: Pipeline,
-        custom_args: %{
-          playlist: %Media{
-            target_segment_duration: 1,
-            uri: playlist_uri
-          },
-          output:
-            {[
-               %Buffer{
-                 payload: "a",
-                 pts: 0,
-                 metadata: %{duration: Membrane.Time.milliseconds(1_500)}
-               }
-             ], &__MODULE__.buffer_generator/2}
-        },
-        test_process: self()
+      playlist = %Media{
+        target_segment_duration: 1,
+        uri: playlist_uri
+      }
+
+      output =
+        {[
+           %Buffer{
+             payload: "a",
+             pts: 0,
+             metadata: %{duration: Membrane.Time.milliseconds(1_500)}
+           }
+         ], &__MODULE__.buffer_generator/2}
+
+      links = [
+        child(:source, %Membrane.Testing.Source{
+          output: output,
+          stream_format: %Membrane.HLS.Format.WebVTT{language: "en-US"}
+        })
+        |> child(:sink, %Membrane.HLS.Sink{
+          playlist: playlist
+        })
       ]
 
-      pipeline = Membrane.Testing.Pipeline.start_link_supervised!(options)
-
-      assert_pipeline_notified(
-        pipeline,
-        :sink,
-        {:write, %{type: :playlist, uri: ^playlist_uri}},
-        1_000
-      )
+      pipeline = Membrane.Testing.Pipeline.start_link_supervised!(structure: links)
 
       segment_uri = URI.new!("s3://bucket/media/00000.vtt")
 
@@ -93,6 +70,15 @@ defmodule Membrane.HLS.SinkTest do
         1_000
       )
 
+      # This comes as soon as segments are complete; in this case "a" lasts for
+      # the whole duration of the first segment and finishes in the second.
+      assert_pipeline_notified(
+        pipeline,
+        :sink,
+        {:write, %{type: :playlist, uri: ^playlist_uri, playlist: %Media{finished: false}}},
+        1_000
+      )
+
       # Assertion on end-of-stream
       assert_pipeline_notified(
         pipeline,
@@ -100,6 +86,12 @@ defmodule Membrane.HLS.SinkTest do
         {:write, %{type: :playlist, uri: ^playlist_uri, playlist: %Media{finished: true}}},
         1_000
       )
+
+      refute_pipeline_notified(pipeline, :sink, {:write, %{type: :segment}}, 1_000)
+      refute_pipeline_notified(pipeline, :sink, {:write, %{type: :playlist}}, 1_000)
+      assert_end_of_stream(pipeline, :sink, :input, 1_000)
+
+      :ok = Membrane.Pipeline.terminate(pipeline, blocking?: true)
     end
   end
 end
