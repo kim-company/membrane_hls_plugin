edd3ef4015d2022fb79c7ebbd3275001b7b559d4
Daniel Morandini <danielmorandini@me.com>
2023-07-18 16:20:30 +0200

    Update membrane_core

--- mix.exs
+++ mix.exs
@@ -23,8 +23,8 @@ defmodule Membrane.HLS.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:membrane_core, "~> 0.11.0"},
-      {:membrane_file_plugin, "~> 0.13.2", only: :test},
+      {:membrane_core, "~> 0.12.0"},
+      {:membrane_file_plugin, "~> 0.14.0", only: :test},
       {:kim_hls, github: "kim-company/kim_hls"},
       {:kim_q, github: "kim-company/kim_q"}
     ]
