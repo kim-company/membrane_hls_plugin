6634e0027a362a3a07466dd897e2db7e4f8ea638
Philip Giuliani <philip.giuliani@keepinmind.info>
2024-09-26 09:58:11 +0200

    Cleanup deps

--- mix.exs
+++ mix.exs
@@ -28,9 +28,8 @@ defmodule Membrane.HLS.MixProject do
       {:membrane_mp4_plugin, "~> 0.35"},
       {:membrane_aac_plugin, "~> 0.18"},
       {:membrane_h26x_plugin, "~> 0.10"},
-      # {:kim_hls, path: "../kim_hls"},
-      {:kim_hls, github: "kim-company/kim_hls", branch: "packager"},
-      {:kim_q, github: "kim-company/kim_q"}
+      {:kim_q, "~> 1.0"},
+      {:kim_hls, github: "kim-company/kim_hls"}
     ]
   end
 
